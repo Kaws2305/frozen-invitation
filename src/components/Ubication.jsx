d11947c4bf6dd2565983b632d85c70bcdbab1880
kaws2305 <joshua.reyes010523@gmail.com>
2025-11-15 21:29:36 -0600

    Segundos cambios

--- src/components/Ubication.jsx
+++ src/components/Ubication.jsx
@@ -17,7 +17,7 @@ export function Ubication () {
                 <div className="ubication__cards-container">
 
                     <Ubicationcards
-                    card__tittle='MISA'
+                    card__tittle='Céremonia Religiosa'
                     card__image='https://lh3.googleusercontent.com/gps-cs-s/AG0ilSymS5pXOwAMJJH8ZKCxCRPQE6wTxXQ6vBlSBKzLOn-MyH3i77_aulSzDeWec6dkYGIAwHwqGqdygeZm1bgbsFYmGNOSacJx6N5dFaJ7CcXXA8tcrO_qpAsCkXXYBa9AybJ4UtAD_g6ksw4j=w289-h312-n-k-no'
                     card__place='CATEDRAL JESÚS SEÑOR DE LA MISERICORDIA' 
                     card__hour='06:30 PM'
@@ -25,7 +25,7 @@ export function Ubication () {
                     card__button='https://maps.app.goo.gl/7RyMR3uAeTxi8gew5' />
 
                     <Ubicationcards
-                    card__tittle='SALÓN'
+                    card__tittle='Recepción'
                     card__image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLekxwEy0XL3foApKrKpG7lCFGEMnM3oKDaw&s'
                     card__place='SALÓN AMERIKA' 
                     card__hour='08:00 PM'
